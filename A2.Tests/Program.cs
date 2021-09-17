b161eaa71fdbe8146c9b87fd4be8dd8ff4b8b145
Alyson De Souza <alysondsouza@yahoo.com>
2021-09-17 12:54:40 +0200

    initial commit

---
+++ A2.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A2.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
