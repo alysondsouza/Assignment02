f826232b1cebae30d37300d8982f36ec38a53e2e
Alyson De Souza <alysondsouza@yahoo.com>
2021-09-20 13:04:39 +0200

    sharing code

--- Assignment2.Test/StudentTest.cs
+++ Assignment2.Test/StudentTest.cs
@@ -9,16 +9,22 @@ namespace Assignment2.Test
         [Fact]
         public void Student_getStatus_givenDateBeforeStart_returnEnumNew()
         {
-            //Student ali = new Student(1, "Ali", "De Souza", Students.getStatus(), 21/09/2021, 21/09/2022, 21/09/2023);
-            Student ali = new Student();
+            DateTime start = new DateTime(2021, 09, 19);
+            DateTime end = new DateTime(2022, 09, 21);
+            DateTime graduation = new DateTime(2023, 09, 21);
+
+            Student ali = new Student(1, "Ali", "De Souza", start, end, graduation);
+            //Student ali = new Student();
             //ali.id = 1;
             //ali.givenName = "Ali";
             //ali.surname = "De Souza";
-            //ali.status = Student.getStatus();
+            //ali.startDate = 2021/09/21;
+            //ali.endDate = 21/09/2022;
+            //ali.graduationDate = 21/09/2021;
 
-            Status expected = new Status();
+            Status expectedNEW = Status.New;
 
-            Assert.Equal(expected, ali.getStatus());
+            Assert.Equal(expectedNEW, ali.getStatus(start, end, graduation));
             //Assert.Equal("1", "2");
         }
     }
