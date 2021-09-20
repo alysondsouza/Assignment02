86cd3d08031b36bf1c4ae87c78ea13e5241ce2bc
Alyson De Souza <alysondsouza@yahoo.com>
2021-09-20 11:47:48 +0200

    A whole new Set-Up of the project

---
+++ Assignment2.Test/StudentTest.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using Assignment2;
+
+namespace Assignment2.Test
+{
+    public class StudentTest
+    {
+        [Fact]
+        public void Student_getStatus_givenDateBeforeStart_returnEnumNew()
+        {
+            //Student ali = new Student(1, "Ali", "De Souza", Students.getStatus(), 21/09/2021, 21/09/2022, 21/09/2023);
+            //Student ali = new Student();
+            //ali.id = 1;
+            //ali.givenName = "Ali";
+            //ali.surname = "De Souza";
+            //ali.status = Student.getStatus();
+
+
+            //Assert.Equal("1", Student.getStatus());
+            Assert.Equal("1", "2");
+        }
+    }
+}
